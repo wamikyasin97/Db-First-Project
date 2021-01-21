a9efe5443995ea0bf9cfe1213e848ec69c831294
Wamik Yasin <wamik.yasin@techverx.com>
2021-01-21 12:17:03 +0500

    first commit

---
+++ Database_First_App/Migrations/20210120143217_first.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database_First_App.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    courseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    courseName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.courseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    studentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    studentName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.studentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ Database_First_App/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database_First_App
+{
+    //Entity classes
+    public class Student
+    {
+        public int studentId { get; set; }
+        public string studentName { get; set; }
+    }
+
+    public class Course
+    {
+        public int courseId { get; set; }
+        public string courseName { get; set; }
+    }
+
+    //Context Class
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=SchoolDB;Trusted_Connection=True;");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+
+                var std = new Student()
+                {
+                    studentName = "Wamik"
+                };
+
+                var ctd = new Course()
+                {
+                    courseName = "ICT"
+                };
+
+                var std1 = new Student()
+                {
+                    studentName = "Wamik1"
+                };
+
+                var ctd1 = new Course()
+                {
+                    courseName = "Computing"
+                };
+
+                var std2 = new Student()
+                {
+                    studentName = "Wamik2"
+                };
+
+                var ctd2 = new Course()
+                {
+                    courseName = "C#"
+                };
+
+                context.Students.Add(std);
+                context.Courses.Add(ctd);
+                context.Students.Add(std1);
+                context.Courses.Add(ctd1);
+                context.Students.Add(std2);
+                context.Courses.Add(ctd2);
+                context.SaveChanges();
+            }
+        }
+    }
+}
